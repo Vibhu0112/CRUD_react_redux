b8754b8a27844a380f3b6e7e3329026c34ab7dd1
Vibhu <vibhudixit0112@gmail.com>
2023-03-07 22:44:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addUser, deleteUser, updateUser } from './components/Users';
+
 
 function App() {
+   const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+
+   const [name, setName] = useState("");
+   const [username, setUsername] = useState("");
+   const [newUsername, setNewUsername] = useState("");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      hello
+      <div className='add'>
+        <input type="text" placeholder="Name...." onChange={(event)=> {setName(event.target.value)}}/>
+        <input type="text" placeholder="UserName...." onChange={(event)=> {setUsername(event.target.value)}}/>
+        <button onClick={() =>{dispatch(addUser({id: userList[userList.length -1].id+1, name , username}))}}> Add User</button>
+
+      </div>
+      <div className='display'>{userList.map((user)=>{
+        return <div> 
+        <div>{user.id}</div>
+        <div>{user.name}</div>
+        <div>{user.username}</div>
+        <input type="text" placeholder="New UserName...." 
+        onChange={(event)=> {setNewUsername(event.target.value)}}
+        />
+        <button onClick={()=> {dispatch(updateUser({id: user.id, username: newUsername}))}}>update</button>
+        <button onClick={()=> {dispatch(deleteUser({id: user.id}))}}>delete</button>
+        </div>
+      })}</div>
+      </div>
   );
 }
 
---
+++ src/components/data.js
@@ -0,0 +1,16 @@
+export const UsersData = [{
+    id:1,
+    name:"vibhu",
+    username:"dixitjii"
+},
+{
+    id:2,
+    name:"vibhu 2",
+    username:"dixitjii 2"
+},
+{
+    id:3,
+    name:"vibhu 3",
+    username:"dixitjii 3"
+},
+];
